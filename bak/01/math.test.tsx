63d7180fb1f41bda821053ef588f0075a7c80f64
Longbao521 <1356078732@qq.com>
2020-05-22 09:16:15 +0800

    Delete bak

--- bak/01/math.test.tsx
+++
@@ -1,8 +0,0 @@
-import { sum } from './math'
-
-it('1+1=2', () => {
-    expect(sum(1,1)).toBe(2)
-})
-it('hello+world1=hello-world', () => {
-    expect(sum('hello','world')).toBe('hello-world')
-})
